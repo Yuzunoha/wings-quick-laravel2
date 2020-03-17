83a0715b4c8d6c896eea3e261d993bdf7d5fb412
Yuzunoha <studysteadystandby@gmail.com>
2020-03-17 11:53:03 +0900

    viewに変数渡すところ

--- html/app/Http/Controllers/HelloController.php
+++ html/app/Http/Controllers/HelloController.php
@@ -8,4 +8,11 @@ class HelloController extends Controller
     {
         return 'こんにちは、世界！';
     }
+    public function view()
+    {
+        $data = [
+            'msg' => 'こんにちは、世界のビュー！',
+        ];
+        return view('hello.view', $data);
+    }
 }
