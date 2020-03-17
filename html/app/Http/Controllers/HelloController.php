9070a630cc9246fe0ca19283f9814ddbbc46816a
Yuzunoha <studysteadystandby@gmail.com>
2020-03-17 11:50:06 +0900

    routingできた

--- html/app/Http/Controllers/HelloController.php
+++ html/app/Http/Controllers/HelloController.php
@@ -2,9 +2,10 @@
 
 namespace App\Http\Controllers;
 
-use Illuminate\Http\Request;
-
 class HelloController extends Controller
 {
-    //
+    public function index()
+    {
+        return 'こんにちは、世界！';
+    }
 }
